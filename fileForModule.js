0cdd1892e0fd0515a10dbb3e7152764f0f73e07d
Pranav M <pranavm265@gmail.com>
2024-05-13 22:52:30 +0530

    lesson 4

--- fileForModule.js
+++ fileForModule.js
@@ -1,5 +1,9 @@
 let people = ['prnv','kevi','joolie'];
+let random = [1,2,3,5,6,4,2]
 
 console.log(people);
 
-module.exports = people; //this returns something to xyz and in this case it is peeeeoooopppleeeee!!!
+module.exports = {
+    people,
+    random
+}; //this returns something to xyz and in this case it is peeeeoooopppleeeee!!! and now random noss
