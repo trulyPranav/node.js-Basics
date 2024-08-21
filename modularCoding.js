c98c55c6d212ee4814a0c5b15d09b8454ce59cba
Pranav M <pranavm265@gmail.com>
2024-08-21 13:19:22 +0530

    os module

--- modularCoding.js
+++ modularCoding.js
@@ -4,3 +4,7 @@ console.log(xyz); //nter kopp eth empty object return akunn
 //ppo empty return akoola hehe cuz import file il export statement add aki now run!
 
 console.log(xyz);
+
+console.log(xyz.people);
+
+console.log(xyz.random);
